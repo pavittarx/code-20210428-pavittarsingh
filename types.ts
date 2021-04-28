43e564d351ee7f6d92da3771a897ce45b8ea918a
pavittarx <29155477+pavittarx@users.noreply.github.com>
2021-04-28 08:38:45 +0530

    typescript config & typings

---
+++ types.ts
@@ -0,0 +1,14 @@
+export type PersonParams = {
+  Gender: string;
+  HeightCm: number;
+  WeightKg: number;
+};
+
+export type HealthProfile = {
+  Gender: string;
+  HeightCm: number;
+  WeightKg: number;
+  BMI: number;
+  BMICategory: string;
+  HealthRisk: string;
+}
